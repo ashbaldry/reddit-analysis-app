5b4f23c79fe73be92d261fe3c98e728f8b87476a
Ashley Baldry <arbaldry91@gmail.com>
2020-12-30 21:59:32 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Reddit Analysis
+
+This application aims to do some top-level analysis into a selected subreddit, including:
+
+- Recent Posts
+- Comment Trends
+- Text Analysis
+
+## Settings
+
+### Reddit API
+In order to use this application, you will need to get a Reddit Client ID/Secret, this can be created 
+[here](https://www.reddit.com/prefs/apps). For more information about the process take a look at Reddit's
+[Wiki page](https://github.com/reddit-archive/reddit/wiki/API).
+
+### Package Information
+This requires the development version of `shiny.semantic` in order to take advantage of [Fomantic UI](https://fomantic-ui.com/)
---
+++ www/sub_search.js
@@ -0,0 +1,75 @@
+var semanticSearchBinding = new Shiny.InputBinding();
+
+$.extend(semanticSearchBinding, {
+
+  // This initialize input element. It extracts data-value attribute and use that as value.
+  initialize: function(el) {
+    $(el).search({
+    type          : 'category',
+    minCharacters : 3,
+    onSelect: function(result, response) { $(el).trigger('select'); },
+    apiSettings   : {
+      onResponse: function(redditResponse) {
+        var response = { results : {} };
+        
+        // translate Reddit API response to work with search
+        $.each(redditResponse.data.children, function(index, item) {
+          var maxResults = 5;
+          
+          if(index >= maxResults) {
+            return false;
+          }
+          
+          response.results[item.data.url] = {
+              name    : item.data.url,
+              results : []
+            };
+          
+          // add result to category
+          response.results[item.data.url].results.push({
+            title       : item.data.url,
+            description : item.data.title
+          });
+        });
+        return response;
+      },
+      url: 'https://www.reddit.com/subreddits/search.json?q={query}&include_over_18=on'
+    }
+  });
+  },
+
+  // This returns a jQuery object with the DOM element.
+  find: function(scope) {
+    return $(scope).find('.search');
+  },
+
+  // Returns the ID of the DOM element.
+  getId: function(el) {
+    return el.id;
+  },
+
+  // Given the DOM element for the input, return the value as JSON.
+  getValue: function(el) {
+    return $(el).search('get value');
+  },
+
+  // Given the DOM element for the input, set the value.
+  setValue: function(el, value) {
+    $(el).search('set exactly', value);
+  },
+
+  // Set up the event listeners so that interactions with the
+  // input will result in data being sent to server.
+  // callback is a function that queues data to be sent to
+  // the server.
+  subscribe: function(el, callback) {
+    $(el).on('select', function () { callback(true); });
+  },
+
+  // TODO: Remove the event listeners.
+  unsubscribe: function(el) {
+    $(el).off('.semanticSearchBinding');
+  }
+});
+
+Shiny.inputBindings.register(semanticSearchBinding, 'shiny.semanticSearch');
