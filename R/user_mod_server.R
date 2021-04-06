f4f16613920521bf5a7ef075b191a25c01f90447
Ashley Baldry <arbaldry91@gmail.com>
2021-04-06 20:36:24 +0100

    Adding in a couple of bits of user info

--- R/user_mod_server.R
+++ R/user_mod_server.R
@@ -8,6 +8,33 @@ user_page_server <- function(input, output, session, reddit, rr) {
     reddit$get_user_info()
   })
   
+  output$user_icon <- renderUI({
+    rr()
+    if (!reddit$is_authorized()) return(tags$img())
+    tags$img(src = reddit$user_info$icon_img)
+  })
+  
+  output$user_name <- renderText({
+    rr()
+    if (!reddit$is_authorized()) return(NULL)
+    reddit$user_info$name
+  })
+  
+  output$account_age <- renderText({
+    rr()
+    if (!reddit$is_authorized()) return(NULL)
+    start_time <- as.Date(as.POSIXct(reddit$user_info$created_utc, origin = "1970-01-01", tz = "UTC"))
+    curr_time <- as.Date(lubridate::with_tz(Sys.time(), tzone = "UTC"))
+    years <- year(curr_time) - year(start_time) - (month(curr_time) < month(start_time))
+    if (as.numeric(curr_time) %% 365 >= as.numeric(start_time) %% 365) {
+      months <- month(curr_time) - month(start_time) - (mday(curr_time) < mday(start_time))
+    } else {
+      months <- 12 - month(curr_time) + month(start_time) - (mday(curr_time) < mday(start_time))
+    }
+    
+    glue::glue("Account Age: {years} year{if (years != 1) 's' else ''} {months} month{if (months != 1) 's' else ''}")
+  })
+  
   #### Post Stats ####
   post_karma <- reactive({
     if (is.null(user_info())) return(NA)
