89b40efaf56734aac4c042b501b07af701ad48a3
Ashley Baldry <arbaldry91@gmail.com>
2021-05-13 21:45:29 +0100

    Adding vote agreement chart

--- R/votes_mod_ui.R
+++ R/votes_mod_ui.R
@@ -18,30 +18,54 @@ votes_page_ui <- function(id) {
       )
     ),
     div(
-      class = "three column row",
+      class = "row",
       div(
-        class = "column",
+        class = "six wide column",
         reddit_segment(
           h2("Information"),
           p("The last 1,000 upvotes and 1,000 downvotes have been pulled from Reddit to determine where you vote the most."),
           p()
-        )
-      ),
-      
-      div(
-        class = "column",
-        reddit_segment(
-          highcharter::highchartOutput(ns("upvote_plt"), height = "330px")
         ),
-        reddit_segment(
-          highcharter::highchartOutput(ns("downvote_plt"), height = "330px")
-        )
-      ),
-      
-      div(
-        class = "column",
+        
         reddit_segment(
           highcharter::highchartOutput(ns("vote_ratio_plt"), height = "450px")
+        )
+      ),
+      
+      div(
+        class = "ten wide column",
+        div(
+          class = "ui grid",
+          div(
+            class = "two column row",
+            div(
+              class = "column",
+              reddit_segment(
+                highcharter::highchartOutput(ns("upvote_plt"), height = "330px")
+              )
+            ),
+            div(
+              class = "column",
+              reddit_segment(
+                highcharter::highchartOutput(ns("downvote_plt"), height = "330px")
+              )
+            ),
+            
+            div(
+              class = "one column row",
+              div(
+                class = "column",
+                div(
+                  class = "ui form",
+                  tags$label("Subreddit:"),
+                  shiny.semantic::dropdown_input(ns("agree_sr"), "All", type = "inline search selection"),
+                ),
+                reddit_segment(
+                  highcharter::highchartOutput(ns("vote_agree_plt"), height = "300px")
+                )
+              )
+            )
+          )
         )
       )
     )
